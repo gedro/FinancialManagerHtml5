575365ecde2f85369ce44dd24a233b2232372f1b
Gabor Harsanyi <harsanyigabor@gmail.com>
2012-12-11 16:55:10 +0200

    Init HTML5 Boilerplate with Twitter Bootstrap, jQuery, jQuery UI, Modernizr, RequireJS, Normalize.css

---
+++ public/js/app.js
@@ -0,0 +1,20 @@
+requirejs.config({
+	baseUrl : 'js/lib',
+	paths : {
+		app : '../app',
+		'jquery' : 'jquery-1.8.3.min',
+		jqueryui : 'jquery-ui-1.9.1/jqueryui',
+		bootstrap : "bootstrap/bootstrap.min",
+		modernizr : 'js/vendor/modernizr-2.6.2.min.js'
+	}
+});
+
+// Start the main app logic.
+require(['modernizr']);
+
+requirejs.onError = function(err) {
+	console.log(err.requireType);
+	console.log('modules: ' + err.requireModules);
+
+	throw err;
+};
